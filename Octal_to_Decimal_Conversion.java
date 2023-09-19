3a1286b12da216d0bb762f6b964203b0612a7c47
LAVANYA CHADARAM <120081620+lavanyachadaram@users.noreply.github.com>
2023-09-19 14:05:19 +0530

    import java.util.Scanner; public class BCV{     public static void main(String[] args){     Scanner bv = new Scanner(System.in); String c = bv.nextLine(); System.out.println(Integer.parseInt(c,8)); }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class BCV{
+    public static void main(String[] args){
+    Scanner bv = new Scanner(System.in);
+String c = bv.nextLine();
+System.out.println(Integer.parseInt(c,8));
+}
+    
+}

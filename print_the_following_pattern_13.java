e3124dde5bbcd9f74e368b53df6bf3c2ada11e76
LAVANYA CHADARAM <120081620+lavanyachadaram@users.noreply.github.com>
2023-09-20 19:21:04 +0530

    import java.util.*; public class praveen{     public static void main(String args[]){     int a; Scanner s=new Scanner(System.in); a=s.nextInt(); for(int i=1;i<=a;i++) {     for(int j=1;j<=i;j++) {     System.out.format("%d",j);
    }
    System.out.println();
    }
        }
    }

---
+++ print_the_following_pattern_13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class praveen{
+    public static void main(String args[]){
+    int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+for(int i=1;i<=a;i++)
+{
+    for(int j=1;j<=i;j++)
+{
+    System.out.format("%d",j);
+
+}
+System.out.println();
+}
+    }
+}
